7eaf8352f36febda79a65736c4e8e17a04e828f7
antoinepirard <antoinepirard@users.noreply.github.com>
2025-07-01 15:35:22 +0200

    Fix deployment try

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,19 +1,8 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import { Theme } from "@radix-ui/themes";
 import { Analytics } from "@vercel/analytics/react";
 import "@radix-ui/themes/styles.css";
 import "./globals.css";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "Antoine Pirard - Design Leader",
@@ -27,9 +16,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable}`}
-      >
+      <body>
         <Theme
           appearance="light"
           accentColor="blue"
