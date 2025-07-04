eb87dbbbc52b50d927f5bf19887da56ba7769548
antoinepirard <antoinepirard@users.noreply.github.com>
2025-07-04 15:30:34 +0200

    fix: adjust TableOfContents positioning and visibility for responsive layouts

--- src/components/TableOfContents.tsx
+++ src/components/TableOfContents.tsx
@@ -69,7 +69,7 @@ export default function TableOfContents({ className = '' }: TableOfContentsProps
 
   return (
     <motion.div
-      className={`fixed left-6 top-1/2 -translate-y-1/2 z-40 ${className}`}
+      className={`fixed left-2 md:left-4 lg:left-6 top-1/2 -translate-y-1/2 z-40 hidden lg:block ${className}`}
       onMouseEnter={() => setIsHovered(true)}
       onMouseLeave={() => setIsHovered(false)}
       initial={{ opacity: 0, x: -20 }}
